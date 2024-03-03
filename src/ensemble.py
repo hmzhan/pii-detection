85d7c30e05a173d1492a48e0f5e5f7c694afc8bb
hmzhan <hmzhan@gmail.com>
2024-03-03 15:35:44 -0500

    add tokenize()

--- src/ensemble.py
+++ src/ensemble.py
@@ -16,5 +16,37 @@ from transformers import (
     TrainingArguments,
     DataCollatorForTokenClassification
 )
+import config
 
+
+def tokenize(example, tokenizer):
+    """
+    Tokenize text in test data
+    :param example: one example from test data
+    :param tokenizer: tokenizer loaded from HuggingFace
+    :return: tokenized text
+    """
+    text = []
+    token_map = []
+    idx = 0
+
+    for t, ws in zip(example["tokens"], example["trailing_whitespace"]):
+        text.append(t)
+        token_map.extend([idx] * len(t))
+        if ws:
+            text.append(" ")
+            token_map.append(-1)
+        idx += 1
+
+    tokenized = tokenizer(
+        "".join(text),
+        return_offsets_mapping=True,
+        trunation=False,
+        max_length=config.INFERENCE_MAX_LENGTH
+    )
+
+    return {
+        **tokenized,
+        "token_map": token_map
+    }
 
